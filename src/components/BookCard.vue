2ec56d7eb39ea48c1cebd849dd51158fdb299c9f
Giacomo202210029 <u202210029@upc.edu.pe>
2025-03-15 16:22:28 -0500

    feat: First Commit

---
+++ src/components/BookCard.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "BookCard.vue"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/BookDetails.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "BookDetails.vue"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
